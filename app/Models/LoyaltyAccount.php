346f561ffc9680bc0c8393bbca8f2b822217f49b
vladChe <kubokat@gmail.com>
2021-12-30 19:37:26 +0500

    Добавил AccountRequest для самых частых случаев валидации, перенес код notify из модели в сервис. Логику из контроллера перенес в сервис

--- app/Models/LoyaltyAccount.php
+++ app/Models/LoyaltyAccount.php
@@ -2,11 +2,7 @@
 
 namespace App\Models;
 
-use App\Mail\AccountActivated;
-use App\Mail\AccountDeactivated;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Mail;
 
 class LoyaltyAccount extends Model
 {
@@ -26,19 +22,4 @@ class LoyaltyAccount extends Model
         return LoyaltyPointsTransaction::where('canceled', '=', 0)->where('account_id', '=', $this->id)->sum('points_amount');
     }
 
-    public function notify()
-    {
-        if ($this->email != '' && $this->email_notification) {
-            if ($this->active) {
-                Mail::to($this)->send(new AccountActivated($this->getBalance()));
-            } else {
-                Mail::to($this)->send(new AccountDeactivated());
-            }
-        }
-
-        if ($this->phone != '' && $this->phone_notification) {
-            // instead SMS component
-            Log::info('Account: phone: ' . $this->phone . ' ' . ($this->active ? 'Activated' : 'Deactivated'));
-        }
-    }
 }
